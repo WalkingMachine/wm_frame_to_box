8924a39074f69d6eb66c1701a7271fa67cb7696f
philippe117 <philippe117@live.fr>
2017-10-12 21:14:37 -0400

    fait tout marcher!  :)

--- src/wm_frame_to_box.cpp
+++ src/wm_frame_to_box.cpp
@@ -9,6 +9,8 @@
 #include <wm_frame_to_box/BoundingBoxes3D.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+#include <std_msgs/String.h>
+#include <tf/transform_listener.h>
 
 darknet_ros_msgs::BoundingBoxes BoundingBoxes2D;
 
@@ -19,32 +21,31 @@ std::string _YOLO_TOPIC = "/darknet_ros/bounding_boxes";
 std::string _CAMERA_FRAME = "head_xtion_depth_frame";
 std::string _BOUNDING_BOXES_TOPIC = "/frame_to_box/bounding_boxes";
 double _DEFAULT_BOX_SIZE = 0.1;
-std::string _BASE_FRAME = "base_link";  // frame of the output
+std::string _BASE_FRAME = "/base_link";  // frame of the output
 
 wm_frame_to_box::BoundingBoxes3D boxes;
 ros::Publisher posePub;
-tf::Transformer transformer;
+//tf::Transformer transformer;
 
-double GetDepth( int x, int y, const sensor_msgs::ImageConstPtr& msg ){
+tf::TransformListener *Listener2;
 
+cv_bridge::CvImagePtr DecodeImage( const sensor_msgs::ImageConstPtr& msg ){
     cv_bridge::CvImagePtr cv_ptr;
     try
     {
         cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_16UC1);
+        return cv_ptr;
     }
     catch (cv_bridge::Exception& e)
     {
         ROS_ERROR("cv_bridge exception: %s", e.what());
-        return -1;
+        return cv_ptr;
     }
-
-    int depth = cv_ptr->image.at<short int>(x, y);
-    ROS_INFO("Depth: %d", depth);
-
+}
+double GetDepth( int x, int y, const cv_bridge::CvImagePtr cv_ptr ){
+    int depth = cv_ptr->image.at<short int>(y, x);
     return depth/1000.0;
 }
-
-
 
 
 // Save the bounding boxes for later
@@ -56,79 +57,82 @@ void DNBB( darknet_ros_msgs::BoundingBoxes msg ){
 // Receive the image and process the depth of the bounding boxes
 void ImageCB(const sensor_msgs::ImageConstPtr& msg){
 
-    boxes.boundingBoxes.clear();
-
     ulong L = BoundingBoxes2D.boundingBoxes.size();
-    for (ulong i=0; i < L; i++ ){
+    if (L>0) {
+        boxes.boundingBoxes.clear();
+        cv_bridge::CvImagePtr ptr = DecodeImage(msg);
 
-        // get frame
-        int xmin = BoundingBoxes2D.boundingBoxes[i].xmin;
-        int xmax = BoundingBoxes2D.boundingBoxes[i].xmax;
-        int ymin = BoundingBoxes2D.boundingBoxes[i].ymin;
-        int ymax = BoundingBoxes2D.boundingBoxes[i].ymax;
+        for (ulong i = 0; i < L; i++) {
+            // get frame
+            int xmin = BoundingBoxes2D.boundingBoxes[i].xmin;
+            int xmax = BoundingBoxes2D.boundingBoxes[i].xmax;
+            int ymin = BoundingBoxes2D.boundingBoxes[i].ymin;
+            int ymax = BoundingBoxes2D.boundingBoxes[i].ymax;
 
-        // get center
-        int x = (xmax+xmin)/2;
-        int y = (ymax+ymin)/2;
+            // get center
+            int x = (xmax + xmin) / 2;
+            int y = (ymax + ymin) / 2;
 
-        // apply limitations
-        if (x<0) x=0;
-        if (x>msg->width) x=msg->width;
-        if (y<0) y=0;
-        if (y>msg->height) y=msg->height;
+            // apply limitations
+            if (x < 0) x = 0;
+            if (x > ptr->image.rows) x = ptr->image.rows;
+            if (y < 0) y = 0;
+            if (y > ptr->image.cols) y = ptr->image.cols;
+            double dist = GetDepth(x, y, ptr);
 
+            // pushing invalid values from the face
+            if (dist < 0.3)
+                dist = 0.3;
 
-        double dist = GetDepth( x, y, msg );
+            // get pixel to rad ratio // TODO check if the dimentions are right
+            double xratio = _CAMERA_ANGLE_WIDTH / msg->width;
+            double yratio = _CAMERA_ANGLE_HEIGHT / msg->height;
 
-//        ROS_INFO("x = %d", x );
-//        ROS_INFO("y = %d", y );
-//        ROS_INFO("dist = %f", dist );
+            // get the IRL angles from the camera center to the object
+            double ax = -((double) x - (double) msg->width / 2) * xratio;  // pixel to angle
+            double ay = -((double) y - (double) msg->height / 2) * yratio;  // pixel to angle
 
-        double xratio = _CAMERA_ANGLE_WIDTH/msg->width;
-        double yratio = _CAMERA_ANGLE_HEIGHT/msg->height;
+            // calculate the relative position in the camera frame.
+            double ry = dist * std::sin(ax);  // ang to 3D point (rad to m)
+            double rz = dist * std::sin(ay);  // ang to 3D point (rad to m)
+            double rx = dist * std::cos(ax) * std::cos(ay);  // ang to 3D point (rad to m)
 
-        double ax = -((double)x-(double)msg->width/2)/xratio;  // pixel to angle
-        double ay = -((double)y-(double)msg->height/2)/yratio;  // pixel to angle
+            // broadcast the boxe to TF
+            auto BoxName = BoundingBoxes2D.boundingBoxes[i].Class;
+            static tf::TransformBroadcaster br;
+            tf::Transform transform;
+            transform.setOrigin(tf::Vector3(rx, ry, rz));
+            tf::Quaternion q;
+            q.setRPY(0, 0, 0);
+            transform.setRotation(q);
+            br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), _CAMERA_FRAME, BoxName));
 
-        double ry = dist*std::sin(ax);  // ang to 3D point (rad to m)
-        double rz = dist*std::sin(ay);  // ang to 3D point (rad to m)
-        double rx = dist*std::cos(ax)*std::cos(ay);  // ang to 3D point (rad to m)
+            // create a box message and fill all the parameters
+            wm_frame_to_box::BoundingBox3D box;
+            box.Class = BoundingBoxes2D.boundingBoxes[i].Class;
 
-        std::string BoxName = BoundingBoxes2D.boundingBoxes[i].Class;
+            // get the center of the box relatively to the base_link using tf
+            tf::StampedTransform tranform;
+            std::string buffer = "/"+BoxName;
+            const char* BoxFrame = buffer.c_str();
+            Listener2->waitForTransform(BoxFrame ,_BASE_FRAME, ros::Time::now(), ros::Duration(0.1));
+            Listener2->lookupTransform( BoxFrame ,_BASE_FRAME, ros::Time(0), tranform);
+            tf::Vector3 origin = tranform.getOrigin();
+            geometry_msgs::Point po;
+            po.x = origin.x();
+            po.y = origin.y();
+            po.z = origin.z();
+            box.Center = po;
 
-        // broadcast the boxe to TF
-        static tf::TransformBroadcaster br;
-        tf::Transform transform;
-        transform.setOrigin( tf::Vector3(rx, ry, rz) );
-        tf::Quaternion q;
-        q.setRPY(0, 0, 0);
-        transform.setRotation(q);
-        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), _CAMERA_FRAME, BoxName));
-
-        // create a box and set all the parameters
-        wm_frame_to_box::BoundingBox3D box;
-        box.Class = BoundingBoxes2D.boundingBoxes[i].Class;
-
-        // get the center of the box
-        tf::StampedTransform tranform;
-        transformer.lookupTransform( _BASE_FRAME, BoxName, ros::Time::now() , tranform );
-        tf::Vector3 origin = tranform.getOrigin();
-        geometry_msgs::Point po;
-        po.x = origin.x();
-        po.y = origin.y();
-        po.z = origin.z();
-        box.Center = po;
-
-        // set the dimentions of the box
-        box.Depth = _DEFAULT_BOX_SIZE;
-        box.Width = _DEFAULT_BOX_SIZE;
-        box.Height = _DEFAULT_BOX_SIZE;
-        box.probability = BoundingBoxes2D.boundingBoxes[i].probability;
-        boxes.boundingBoxes.push_back(box);
-        posePub.publish(boxes);
-
+            // set the dimentions of the box
+            box.Depth = _DEFAULT_BOX_SIZE;
+            box.Width = _DEFAULT_BOX_SIZE;
+            box.Height = _DEFAULT_BOX_SIZE;
+            box.probability = BoundingBoxes2D.boundingBoxes[i].probability;
+            boxes.boundingBoxes.push_back(box);
+            posePub.publish(boxes);
+        }
     }
-
     BoundingBoxes2D.boundingBoxes.clear();
 
 }
@@ -139,7 +143,10 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "frame_to_box");
 
     ros::NodeHandle nh;
+    tf::TransformListener Listener;
+    Listener2 = &Listener;
 
+    // get all parameters
     nh.getParam("camera_angle_width", _CAMERA_ANGLE_WIDTH);
     nh.getParam("camera_angle_height", _CAMERA_ANGLE_HEIGHT);
     nh.getParam("camera_topic", _CAMERA_TOPIC);
@@ -149,14 +156,18 @@ int main(int argc, char **argv) {
     nh.getParam("camera_frame", _CAMERA_FRAME);
     nh.getParam("base_frame", _BASE_FRAME);
 
+    // subscribe to the camera topic
     image_transport::ImageTransport it(nh);
     image_transport::Subscriber sub = it.subscribe(_CAMERA_TOPIC, 1, ImageCB);
 
+    // subscribe to the yolo topic
     ros::Subscriber bbsub = nh.subscribe(_YOLO_TOPIC, 1, DNBB);
 
+    // advertise the box3D topic
     posePub = nh.advertise<wm_frame_to_box::BoundingBoxes3D>( _BOUNDING_BOXES_TOPIC, 10 );
 
-    ros::Rate period(0.1);  // 10 Hz
+    // run run run!
+    ros::Rate period(10);  // 10 Hz
     while( ros::ok() ){
         ros::spinOnce();
         period.sleep();
